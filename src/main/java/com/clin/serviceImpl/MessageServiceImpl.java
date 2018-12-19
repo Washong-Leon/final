29dad1313ea3c0602a8db6106ea71a88cfe44110
Washong-Leon <44047029+Washong-Leon@users.noreply.github.com>
2018-12-19 10:43:37 +0800

    Add files via upload

--- src/main/java/com/clin/serviceImpl/MessageServiceImpl.java
+++ src/main/java/com/clin/serviceImpl/MessageServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 import java.util.List;
 
-@Service("MessageService")
+@Service("messageService")
 public class MessageServiceImpl implements MessageService {
     @Resource
     private MessageDao messageDao;
